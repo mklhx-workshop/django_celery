bb158840af40cd4422e5d246cec92cc710370279
Mickael <mickael.lehoux@greenponik.com>
2020-11-20 12:08:04 +0100

    some fixes

--- django_app/core/urls.py
+++ django_app/core/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from . import views
+import django_celery.settings as settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.index, name="core-index"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
